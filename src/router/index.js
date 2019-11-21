9a14a84fcca63715871ed4b9b61bee2a71a84490
Administrator <1796638470@qq.com>
2019-11-21 10:47:29 +0800

    关于我的模块所有的路由和页面

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Main from '@/components/common/Main'
 import BookList from '@/views/book/BookList'
+import packagingInformationInput from '@/components/common/packagingInformationInput'
+import packagingInformationQuery from '@/components/common/packagingInformationQuery'
+import inventoryManagement from '@/components/common/inventoryManagement'
+import outboundManagement from '@/components/common/outboundManagement'
+import warehousingManagement from '@/components/common/warehousingManagement'
+import packagingMaterialManagement from '@/components/common/packagingMaterialManagement'
+
 
 Vue.use(Router)
 
@@ -14,6 +21,43 @@ export default new Router({
         path: '/book/BookList',
         name: 'BookList',
         component: BookList
-      }]
-  }]
+        },
+        {
+        path: '/packagingInformationInput',
+        name: 'packagingInformationInput',
+        component: packagingInformationInput
+        },
+        {
+        path: '/packagingInformationQuery',
+        name: 'packagingInformationQuery',
+        component: packagingInformationQuery
+        },
+        {
+        path: '/inventoryManagement',
+        name: 'inventoryManagement',
+        component: inventoryManagement
+        },
+        {
+        path: '/outboundManagement',
+        name: 'outboundManagement',
+        component: outboundManagement
+        },
+        {
+        path: '/warehousingManagement',
+        name: 'warehousingManagement',
+        component: warehousingManagement
+        },
+        {
+        path: '/packagingMaterialManagement',
+        name: 'packagingMaterialManagement',
+        component: packagingMaterialManagement
+        }
+
+
+
+
+
+
+        ]
+    }]
 })
